5b4cf861704c7354e91bac02b341bda27637d597
kilb <github@bll.io>
2017-12-29 15:54:47 +0800

    add code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # dblp
+
+## 参数说明
+
+n1: 最少在该会议发过n1篇论文才算该会议的支持者
+
+n2, n3: 最少最近n2年在该会议发过n3篇文章才算活跃
+
+n4: 在11年最少合作过n4次才算做团体
+
+n5: 在前5年最少合作过n5次才算是团体
+
+n6: 在后六年最少合作过n6次才算是团体
+
+## 输出说明
+
+suport.json: 各个会议的支持者
+
+active.json: 依然活跃者
+
+no_active.json: 不再活跃者
+
+groups: 团体
+
+co_auths.json: 团体的详细数据，[团队1，[[conference id，year id, title, 序号]，[团队1， conference id，year id, title, 序号]]]
+
+co_auths_pre.json: 前5年团体的详细数据，格式同上
+
+co_auths_post.json: 后6年团体的详细数据，格式同上
--- main.py
+++ main.py
@@ -1,62 +1,82 @@
 import operator
 import pickle
 import pyfpgrowth
+import json
 from functools import reduce
 from collections import Counter
 
-lines = open('data/FilteredDBLP.txt','r').read().splitlines()
-lines = lines[1:] + lines[0:1]
+n1 = 5
+n2 = 2
+n3 = 2
+n4 = 4 # min suport
+n5 = 3 # min suport for first five years
+n6 = 4 # min suport for last six years
 
-auth_dict = {}
-conf_dict = {'IJCAI': 0, 'AAAI': 1, 'COLT': 2, 'CVPR': 3, 'NIPS': 4, 'KR': 5, 'SIGIR': 6, 'KDD': 7}
-year_dict = {'2007': 0, '2008': 1, '2009': 2, '2010': 3, '2011': 4, '2012': 5, '2013': 6, '2014': 7, '2015': 8, '2016': 9, '2017': 10}
+with open('data/dictionary.json', 'r') as f:
+    dicts = json.load(f)
 
-co_auths = []
-mat = [[[] for x in range(len(year_dict))] for y in range(len(conf_dict))]
-tmp = []
-i = 0
-j = 0
-c = 1
+with open('data/data_transformed.json', 'r') as f:
+    data = json.load(f)
 
-for line in lines:
-    if line[0] == '#':
-        if i < 8:
-            tmp.sort()
-            mat[i][j] = mat[i][j] + tmp
-            co_auths.append(tmp)
-            tmp = []
-        continue
+# lines = open('data/FilteredDBLP.txt','r').read().splitlines()
+# lines = lines[1:] + lines[0:1]
+
+auth_dict = dicts[0]
+conf_dict = dicts[1]
+year_dict = dicts[2]
+
+for i in range(len(data)):
+    data[i].append(i)
+
+# co_auths = []
+# mat = [[[] for x in range(len(year_dict))] for y in range(len(conf_dict))]
+# tmp = []
+# i = 0
+# j = 0
+# c = 1
+
+# for line in lines:
+#     if line[0] == '#':
+#         if i < 8:
+#             tmp.sort()
+#             mat[i][j] = mat[i][j] + tmp
+#             co_auths.append(tmp)
+#             tmp = []
+#         continue
     
-    words = line.split('\t')
+#     words = line.split('\t')
     
-    if words[0] == 'author':
-        if words[1] not in auth_dict:
-            auth_dict[words[1]] = c
-            c += 1
-        tmp.append(auth_dict[words[1]])
+#     if words[0] == 'author':
+#         if words[1] not in auth_dict:
+#             auth_dict[words[1]] = c
+#             c += 1
+#         tmp.append(auth_dict[words[1]])
     
-    elif words[0] == 'year':
-        j = year_dict[words[1]]
-    elif words[0] == 'Conference':
-        w = words[1].split('@')[1] if '@' in words[1] else words[1] 
-        i = conf_dict[w] if w in conf_dict else 8
+#     elif words[0] == 'year':
+#         j = year_dict[words[1]]
+#     elif words[0] == 'Conference':
+#         w = words[1].split('@')[1] if '@' in words[1] else words[1] 
+#         i = conf_dict[w] if w in conf_dict else 8
 
 auth_dict_re = {}
+conf_dict_re = {}
 
 for k, v in auth_dict.items():
     auth_dict_re[v] = k
 
-print(c)
+for k, v in conf_dict.items():
+    conf_dict_re[v] = k
+
+# print(c)
 
 auths_by_year = [[{} for x in range(len(year_dict))] for y in range(len(conf_dict))]
 
-for i in range(len(mat)):
-    for j in range(len(mat[i])):
-        for v in mat[i][j]:
-            if v not in auths_by_year[i][j]:
-                auths_by_year[i][j][v] = 1
-            else:
-                auths_by_year[i][j][v] += 1
+for v in data:
+    for a in v[0]:
+        if a not in auths_by_year[v[1]][v[2]]:
+            auths_by_year[v[1]][v[2]][a] = 1
+        else:
+            auths_by_year[v[1]][v[2]][a] += 1
 
 auths = list(map(lambda x: reduce(lambda y,z: dict(Counter(y)+Counter(z)), x ), auths_by_year))
 
@@ -64,10 +84,6 @@ sorted_auths_by_year = list(map(lambda y: list(map(lambda x: sorted(x.items(), k
 
 sorted_auths = list(map(lambda x: sorted(x.items(), key=operator.itemgetter(1), reverse=True), auths))
 
-n1 = 10
-n2 = 2
-n3 = 5
-n4 = 4
 
 filter_auths = list(map(lambda x: list(filter(lambda y: y[1] > n1, x)), sorted_auths))
 
@@ -84,27 +100,86 @@ for i in range(len(filter_auths)):
             tmp.append(v)
     active_auths.append(tmp)
 
-patterns = pyfpgrowth.find_frequent_patterns(co_auths, n4)
+non_active = list(map(lambda x: list(filter(lambda y: y not in active_auths, x)), filter_auths))
 
-co_patterns = dict(filter(lambda x: len(x[0]) > 1, patterns.items()))
+def gen_out(x):
+    out_dict = {}
+    for i in range(len(x)):
+        out_dict[conf_dict_re[i]] = list(map(lambda y: auth_dict_re[y[0]], x[i]))
+        print(conf_dict_re[i], len(out_dict[conf_dict_re[i]]))
+        
+    return out_dict
 
-co_patterns = sorted(co_patterns.items(), key = lambda x: len(x[0]), reverse=True)
+print("all: ",)
+out_suport = gen_out(filter_auths)
+print("active: ",)
+out_active = gen_out(active_auths)
+print("non-active: ",)
+out_non_active = gen_out(non_active)
 
-# remove duplicate
-pats = []
+#----group-----
+def get_groups(x, n):
+    co_auths = [y[0] for y in x]
+    patterns = pyfpgrowth.find_frequent_patterns(co_auths, n)
+    co_patterns = dict(filter(lambda x: len(x[0]) > 2, patterns.items()))
+    co_patterns = sorted(co_patterns.items(), key = lambda x: len(x[0]), reverse=True)
 
-def is_duplicate(x):
-    for v in pats:
-        if set(x) < v:
-            return False
-    pats.append(set(x))
-    return True
+    # remove duplicate
+    pats = []
+    def is_duplicate(a):
+        for v in pats:
+            if set(a) < v:
+                return False
+        pats.append(set(a))
+        return True
 
-print(len(co_patterns))
-co_patterns = list(filter(lambda x: is_duplicate(x[0]), co_patterns))
+    co_patterns = list(filter(lambda y: is_duplicate(y[0]), co_patterns))
+    print(len(co_patterns))
+    co_patterns = list(map(lambda y: list(y[0]), co_patterns))
 
+    def get_details(a):
+        tmp = []
+        for v in x:
+            if set(a) < set(v[0]):
+                tmp.append(v[1:])
+        return [a,tmp]
 
-print(len(co_patterns))
+    co_patterns = list(map(lambda y: get_details(y), co_patterns))           
+    return co_patterns
+
+data_pre = list(filter(lambda x: x[2] < 5, data))
+data_post = list(filter(lambda x: x[2] >= 5, data))
+
+print("11 years groups number:",)
+co_auths = get_groups(data, n4)
+print("first five years groups number:",)
+co_auths_pre = get_groups(data_pre, n5)
+print("last six years groups number:",)
+co_auths_post = get_groups(data_post, n6)
+
+co_out = list(map(lambda x: list(map(lambda y: auth_dict_re[y], x[0])), co_auths))
+
+# -----output------
+with open('output/suport.json', 'w') as f:
+    json.dump(out_suport, f)
+
+with open('output/active.json', 'w') as f:
+    json.dump(out_active, f)
+
+with open('output/non_active.json', 'w') as f:
+    json.dump(out_non_active, f)
+
+with open('output/co_auths.json', 'w') as f:
+    json.dump(co_auths, f)
+
+with open('output/co_auths_pre.json', 'w') as f:
+    json.dump(co_auths_pre, f)
+
+with open('output/co_auths_post.json', 'w') as f:
+    json.dump(co_auths_post, f)
+
+with open('output/groups.json', 'w') as f:
+    json.dump(co_out, f)
 
 # rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
 
